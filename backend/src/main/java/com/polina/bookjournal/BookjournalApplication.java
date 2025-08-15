59d6b3a69cf2facbec02b579aef2326e3920d822
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2025-08-15 09:01:25 +0200

    created spring boot project

---
+++ backend/src/main/java/com/polina/bookjournal/BookjournalApplication.java
@@ -0,0 +1,13 @@
+package com.polina.bookjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookjournalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/polina/bookjournal/BookjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polina.bookjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
