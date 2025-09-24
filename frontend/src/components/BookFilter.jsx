1fb96953236ab150a31ba75cc6e3bbcc8a9d6656
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2025-09-24 20:54:56 +0200

    added filters

---
+++ frontend/src/components/BookFilter.jsx
@@ -0,0 +1,29 @@
+export default function BookFilter({ filters, setFilters }) {
+    return (
+    <div className="book-filters" >
+    <input
+        type="text"
+        placeholder="Search by title"
+        value={filters.title}
+        onChange={(e) => setFilters({ ...filters, title: e.target.value })}
+    />
+    <input
+        type="text"
+        placeholder="Search by author"
+        value={filters.author}
+        onChange={(e) => setFilters({ ...filters, author: e.target.value })}
+    />
+    <select
+        value={filters.status}
+        onChange={(e) => setFilters({ ...filters, status: e.target.value })}
+    >
+        <option value="">All statuses</option>
+        <option value="TO_READ">To Read</option>
+        <option value="READING">Reading</option>
+        <option value="FINISHED">Finished</option>
+        <option value="DROPPED">Dropped</option>
+    </select>
+
+    </div>
+    );
+    }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import BookList from '../components/BookList';
 import AddBookModal from '../components/AddBookModal';
 import { getBooks } from '../services/bookService';
+import BookFilter from '../components/BookFilter';
 
 
 export default function Home() {
 const [books, setBooks] = useState([]);
 const [showModal, setShowModal] = useState(false);
+const [filters, setFilters] = useState({title: '', author: '', status: ''});
 
 
   const fetchBooks = async () => {
@@ -18,12 +20,21 @@ const [showModal, setShowModal] = useState(false);
     fetchBooks();
   }, []);
 
+  const filteredBooks = books.filter((book) => {
+    return (
+      book.title.toLowerCase().includes(filters.title.toLowerCase()) &&
+      book.author.toLowerCase().includes(filters.author.toLowerCase()) &&
+      (filters.status === '' || book.status === filters.status)
+    );
+  });
+
 
 return (
 
     <div>
-       <button onClick={() => setShowModal(true)}>➕</button>
-       <BookList books={books} />
+      <button onClick={() => setShowModal(true)}>➕</button>
+      <BookFilter filters = {filters} setFilters={setFilters} />
+      <BookList books={filteredBooks} />
       {showModal && (
         <AddBookModal
           onClose={() => setShowModal(false)}
